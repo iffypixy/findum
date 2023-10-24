47f1d355eb8b013fc79506adb01696afa41c1662
Ansat Euler <iffypixy@gmail.com>
2023-10-24 20:21:54 +0200

    feat: add css vars

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -2,7 +2,38 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: {
+          200: "var(--color-main-200)",
+          300: "var(--color-main-300)",
+          400: "var(--color-main-400)",
+          500: "var(--color-main-500)",
+          600: "var(--color-main-600)",
+          700: "var(--color-main-700)",
+          800: "var(--color-main-800)",
+          900: "var(--color-main-900)",
+          DEFAULT: "var(--color-main-500)",
+          contrast: "rgb(var(--color-main-contrast) / <alpha-value>)",
+        },
+        accent: {
+          200: "var(--color-accent-200)",
+          300: "var(--color-accent-300)",
+          400: "var(--color-accent-400)",
+          500: "var(--color-accent-500)",
+          600: "var(--color-accent-600)",
+          700: "var(--color-accent-700)",
+          800: "var(--color-accent-800)",
+          900: "var(--color-accent-900)",
+          DEFAULT: "var(--color-accent-500)",
+          contrast: "var(--color-accent-contrast)",
+        },
+        paper: {
+          DEFAULT: "var(--color-paper)",
+          contrast: "rgb(var(--color-paper-contrast) / <alpha-value>)",
+        },
+      },
+    },
     screens: {
       "2xl": {
         max: "1280px",
