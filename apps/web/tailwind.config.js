7b9d64338bae2dfeeb6a5ddbb5c9f73e63e820ff
Ansat Euler <iffypixy@gmail.com>
2023-10-28 13:17:09 +0200

    feat: add paper brand color

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -32,6 +32,7 @@ export default {
           contrast: "var(--color-accent-contrast)",
         },
         paper: {
+          brand: "var(--color-paper-brand)",
           DEFAULT: "var(--color-paper)",
           contrast: "rgb(var(--color-paper-contrast) / <alpha-value>)",
         },
