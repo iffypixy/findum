a1b35dbeb1baf6f77fd3765dd8b78487f9bc55df
Ansat Euler <iffypixy@gmail.com>
2023-11-06 14:44:19 +0200

    feat: add theming lib

---
+++ apps/web/src/shared/lib/theming/index.tsx
@@ -0,0 +1,33 @@
+import {useEffect} from "react";
+import {create} from "zustand";
+
+type Theme = "light" | "dark";
+
+interface ThemingStore {
+  theme: Theme;
+}
+
+export const useThemingStore = create<ThemingStore>((set) => ({
+  theme: "light",
+  setTheme: (theme: Theme) => set({theme}),
+  toggleTheme: () =>
+    set(({theme}) => {
+      const toggled: Theme = theme === "light" ? "dark" : "light";
+
+      return {theme: toggled};
+    }),
+}));
+
+interface ThemeToggleProps {
+  children?: React.ReactNode;
+}
+
+export const ThemeManager: React.FC<ThemeToggleProps> = ({children}) => {
+  const theme = useThemingStore((state) => state.theme);
+
+  useEffect(() => {
+    document.documentElement.dataset.theme = theme;
+  }, [theme]);
+
+  return children;
+};
