2ad2c3f7eaaeef34deefaee2296c5110bda0dc83
Ansat Euler <iffypixy@gmail.com>
2023-11-06 15:11:25 +0200

    feat: add avatar

---
+++ apps/web/src/shared/ui/atoms/avatar.tsx
@@ -0,0 +1,14 @@
+import {cx} from "class-variance-authority";
+
+import {PropsWithClassName} from "@shared/lib";
+
+interface AvatarProps extends PropsWithClassName {
+  src: string;
+  alt?: string;
+}
+
+export const Avatar: React.FC<AvatarProps> = ({src, alt, className}) => (
+  <div className={cx("w-12 h-12 rounded-full overflow-hidden", className)}>
+    <img className="w-[100%] h-full" src={src} alt={alt} />
+  </div>
+);
