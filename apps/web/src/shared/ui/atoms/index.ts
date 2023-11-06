37ef0c4d17f33665649ea3b08ec4c6be924ee8c5
Ansat Euler <iffypixy@gmail.com>
2023-11-06 15:29:11 +0200

    chore: enable export barrel

---
+++ apps/web/src/shared/ui/atoms/index.ts
@@ -0,0 +1,7 @@
+export {Avatar} from "./avatar";
+export {Button} from "./button";
+export {TextField, UploadField} from "./input";
+export {Checkbox} from "./checkbox";
+export {Link} from "./link";
+export {Textarea} from "./textarea";
+export * from "./heading";
