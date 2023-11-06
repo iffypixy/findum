0dba6fe509fb44e51185068244b1455bbc9c4790
Ansat Euler <iffypixy@gmail.com>
2023-11-06 14:59:12 +0200

    chore: enable export barrel

---
+++ apps/web/src/shared/ui/icons/index.ts
@@ -0,0 +1,8 @@
+import Rooms from "./rooms.svg?react";
+import Image from "./image.svg?react";
+import Cross from "./cross.svg?react";
+import Resume from "./resume.svg?react";
+import Success from "./success.svg?react";
+import * as Flag from "./flags";
+
+export const Icon = {Rooms, Image, Flag, Success, Cross, Resume};
