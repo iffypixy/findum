4d439585d6e7378fbfa70cf021db61a644a5eaf9
Ansat Euler <iffypixy@gmail.com>
2023-11-12 19:16:56 +0100

    feat: add width transition

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      transitionProperty: {
+        width: "width",
+      },
       fontSize: {
         inherit: "inherit",
       },
