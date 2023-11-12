9f8dc328c29657bdbad3947981d07cbc8ba5667c
Ansat Euler <iffypixy@gmail.com>
2023-11-12 13:05:37 +0100

    feat: add select

---
+++ apps/web/src/shared/ui/atoms/select.tsx
@@ -0,0 +1,89 @@
+import {PropsWithChildren} from "react";
+import * as S from "@radix-ui/react-select";
+
+import {PropsWithClassName} from "@shared/lib/types";
+import {Icon} from "@shared/ui";
+import {cx} from "class-variance-authority";
+
+interface SelectRootProps
+  extends React.ComponentProps<typeof S.Root>,
+    PropsWithClassName,
+    PropsWithChildren {
+  placeholder?: string;
+  label?: string;
+  error?: string;
+}
+
+const Root: React.FC<SelectRootProps> = ({
+  placeholder,
+  label,
+  error,
+  children,
+  className,
+  ...props
+}) => {
+  const select = (
+    <S.Root {...props}>
+      <S.Trigger
+        className="
+          w-[100%] flex flex-col space-y-1 rounded-lg border border-paper-contrast/40 focus:border-accent data-[placeholder]:text-paper-contrast/40"
+      >
+        <span className="w-[100%] h-12 flex justify-between items-center rounded-lg py-2 px-4">
+          <S.Value placeholder={placeholder} />
+        </span>
+      </S.Trigger>
+
+      <S.Portal>
+        <S.Content className="w-fit bg-paper rounded-md border border-paper-contrast shadow-md">
+          <S.ScrollUpButton className="flex items-center justify-center bg-paper text-paper-contrast cursor-default">
+            <Icon.Chevron.Up className="w-[1.5rem] h-auto" />
+          </S.ScrollUpButton>
+
+          <S.Viewport className="p-2">{children}</S.Viewport>
+
+          <S.ScrollDownButton className="flex items-center justify-center bg-paper text-paper-contrast cursor-default">
+            <Icon.Chevron.Down className="w-[1.5rem] h-auto" />
+          </S.ScrollDownButton>
+        </S.Content>
+      </S.Portal>
+    </S.Root>
+  );
+
+  return (
+    <div className={cx("h-24 flex flex-col space-y-1", className)}>
+      {label ? (
+        <label className="flex flex-col space-y-1 text-left">
+          <span className="text-paper-contrast/80">{label}</span>
+
+          {select}
+        </label>
+      ) : (
+        select
+      )}
+
+      {error && <span className="text-error text-sm">{error}</span>}
+    </div>
+  );
+};
+
+interface SelectItemProps {
+  value: string;
+  children?: React.ReactNode;
+}
+
+const Item: React.FC<SelectItemProps> = ({value, children}) => {
+  return (
+    <S.Item
+      value={value}
+      className="flex items-center space-x-2 data-[highlighted]:bg-accent data-[highlighted]:text-accent-contrast text-sm rounded-md select-none outline-none p-2"
+    >
+      <S.ItemIndicator className="inline-flex items-center justify-center">
+        <Icon.Check className="w-[1.5rem] h-auto" />
+      </S.ItemIndicator>
+
+      <S.ItemText>{children}</S.ItemText>
+    </S.Item>
+  );
+};
+
+export const Select = {Root, Item};
