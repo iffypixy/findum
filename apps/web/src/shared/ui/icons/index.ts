4f23b8d8698cb33e43dde85b4d921a3bc61ac78a
Ansat Euler <iffypixy@gmail.com>
2023-11-12 13:07:48 +0100

    chore: extend export barrel

--- apps/web/src/shared/ui/icons/index.ts
+++ apps/web/src/shared/ui/icons/index.ts
@@ -3,6 +3,17 @@ import Image from "./image.svg?react";
 import Cross from "./cross.svg?react";
 import Resume from "./resume.svg?react";
 import Success from "./success.svg?react";
+import Check from "./check.svg?react";
+import * as Chevron from "./chevron";
 import * as Flag from "./flags";
 
-export const Icon = {Rooms, Image, Flag, Success, Cross, Resume};
+export const Icon = {
+  Rooms,
+  Image,
+  Flag,
+  Success,
+  Cross,
+  Resume,
+  Check,
+  Chevron,
+};
