76b40c72bb2ea2094609b63ff4557c4d3e4e7fdd
Ansat Euler <iffypixy@gmail.com>
2023-11-20 23:57:10 +0100

    refactor: update date-picker

--- apps/web/src/shared/ui/organisms/date-picker/index.tsx
+++ apps/web/src/shared/ui/organisms/date-picker/index.tsx
@@ -6,7 +6,7 @@ import {cx} from "class-variance-authority";
 import {AiOutlineCalendar} from "react-icons/ai";
 import dayjs from "dayjs";
 
-import {Nullable} from "@shared/lib/types";
+import {Nullable, PropsWithClassName} from "@shared/lib/types";
 
 import "react-day-picker/dist/style.css";
 import "./index.css";
@@ -19,11 +19,12 @@ interface DatePickerProps
   onSelect?: (date: Date) => void;
 }
 
-export const DatePicker: React.FC<DatePickerProps> = ({
+export const DatePicker: React.FC<DatePickerProps & PropsWithClassName> = ({
   error,
   label,
   initialValue,
   onSelect,
+  className,
   ...props
 }) => {
   const [open, setOpen] = useState(false);
@@ -35,10 +36,10 @@ export const DatePicker: React.FC<DatePickerProps> = ({
 
   const trigger = (
     <Popover.Trigger>
-      <div
+      <span
         className={twMerge(
           cx(
-            "flex items-center border border-paper-contrast/25 rounded-md space-x-2 transition p-2",
+            "relative flex items-center border border-paper-contrast/25 rounded-md space-x-2 transition p-2",
             {
               "border-accent": open,
             },
@@ -60,18 +61,18 @@ export const DatePicker: React.FC<DatePickerProps> = ({
 
         <AiOutlineCalendar
           className={twMerge(
-            cx("w-5 h-auto fill-paper-contrast/25", {
+            cx("w-5 h-auto absolute right-2 top-2.5 fill-paper-contrast/25", {
               "fill-accent": open,
             }),
           )}
         />
-      </div>
+      </span>
     </Popover.Trigger>
   );
 
   return (
     <Popover.Root open={open} onOpenChange={setOpen}>
-      <div className="flex flex-col space-y-1">
+      <div className={cx("flex flex-col space-y-1", className)}>
         {label ? (
           <label className="flex flex-col space-y-1 text-left">
             <span className="text-paper-contrast/80">{label}</span>
@@ -86,7 +87,7 @@ export const DatePicker: React.FC<DatePickerProps> = ({
       </div>
 
       <Popover.Portal>
-        <Popover.Content>
+        <Popover.Content className="z-[999]">
           <DayPicker
             {...props}
             mode="single"
