a47e3bd4289c00ddebeaf838a49a776d23f9a5b2
Ansat Euler <iffypixy@gmail.com>
2024-01-02 23:06:18 +0100

    chore: add request interceptor

--- apps/web/src/shared/lib/request/index.ts
+++ apps/web/src/shared/lib/request/index.ts
@@ -5,3 +5,9 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 export const request = axios.create({
   baseURL: BACKEND_URL,
 });
+
+request.interceptors.request.use(function (config) {
+  config.headers.Authorization = localStorage.getItem("token");
+
+  return config;
+});
