cd32c93cd0850d103edc508d1d09e2737b8c0721
Ansat Euler <iffypixy@gmail.com>
2024-01-02 23:07:00 +0100

    chore: made general fix

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,9 +1,12 @@
 import ReactDOM from "react-dom/client";
 import * as Toast from "@radix-ui/react-toast";
 import {ReactKeycloakProvider} from "@react-keycloak/web";
+import {Provider} from "react-redux";
 
 import {keycloak} from "@shared/lib/keycloak";
 import {RobokassaLoader} from "@shared/lib/robokassa";
+import {store} from "@shared/lib/store";
+import {CredentialsLoader} from "@features/auth";
 
 import {App} from "./app";
 
@@ -12,16 +15,22 @@ import "./index.css";
 const root = document.getElementById("root")!;
 
 ReactDOM.createRoot(root).render(
-  <ReactKeycloakProvider
-    authClient={keycloak}
-    initOptions={{checkLoginIframe: false}}
-  >
-    <RobokassaLoader>
-      <Toast.Provider swipeDirection="right">
-        <Toast.Viewport className="fixed top-0 right-0 p-10" />
+  <Provider store={store}>
+    <ReactKeycloakProvider
+      authClient={keycloak}
+      initOptions={{
+        checkLoginIframe: false,
+      }}
+    >
+      <CredentialsLoader>
+        <RobokassaLoader>
+          <Toast.Provider swipeDirection="right">
+            <Toast.Viewport className="fixed top-0 right-0 p-10" />
 
-        <App />
-      </Toast.Provider>
-    </RobokassaLoader>
-  </ReactKeycloakProvider>,
+            <App />
+          </Toast.Provider>
+        </RobokassaLoader>
+      </CredentialsLoader>
+    </ReactKeycloakProvider>
+  </Provider>,
 );
