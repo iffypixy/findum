9f045e2867efddcbc3a322d8e46705e6755467cf
Ansat Euler <iffypixy@gmail.com>
2024-01-06 11:47:39 +0100

    chore: enable session

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,12 +1,16 @@
 import {NestFactory} from "@nestjs/core";
 import {ValidationPipe} from "@nestjs/common";
 
+import {session} from "@lib/session";
+
 import {AppModule} from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use(session);
 
   await app.listen(3000);
 }
