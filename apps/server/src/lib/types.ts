712ac870929e8da73f241723f64a77c3edc5a43d
Ansat Euler <iffypixy@gmail.com>
2024-01-07 17:17:10 +0100

    feat: add more common types

--- apps/server/src/lib/types.ts
+++ apps/server/src/lib/types.ts
@@ -10,3 +10,45 @@ export type CompleteUser = Prisma.UserGetPayload<{
     profile: true;
   };
 }>;
+
+export type CompleteReview = Prisma.ReviewGetPayload<{
+  include: {
+    project: {
+      include: {
+        founder: true;
+      };
+    };
+  };
+}>;
+
+export type CompleteProjectCard = Prisma.ProjectCardGetPayload<{
+  include: {
+    members: {
+      include: {
+        user: true;
+      };
+    };
+    project: {
+      include: {
+        founder: true;
+      };
+    };
+  };
+}>;
+
+export type CompleteProject = Prisma.ProjectGetPayload<{
+  include: {
+    founder: true;
+    members: {
+      include: {
+        user: true;
+      };
+    };
+  };
+}>;
+
+export type CompleteProjectMember = Prisma.ProjectMemberGetPayload<{
+  include: {
+    user: true;
+  };
+}>;
