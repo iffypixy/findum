36e50586f5fe72cfb8ec3eec8cb1ff899018bb4f
Ansat Euler <iffypixy@gmail.com>
2024-01-07 17:15:48 +0100

    feat: add socket lib

---
+++ apps/server/src/lib/socket/index.ts
@@ -0,0 +1,2 @@
+export {SocketModule} from "./socket.module";
+export {SocketService} from "./socket.service";
---
+++ apps/server/src/lib/socket/socket.module.ts
@@ -0,0 +1,11 @@
+import {Module, Global} from "@nestjs/common";
+
+import {SocketService} from "./socket.service";
+
+@Global()
+@Module({
+  controllers: [],
+  providers: [SocketService],
+  exports: [SocketService],
+})
+export class SocketModule {}
