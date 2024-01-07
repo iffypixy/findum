aeb47b09ac45b5b81d0c100b55ad4fed8c1954e6
Ansat Euler <iffypixy@gmail.com>
2024-01-07 17:17:28 +0100

    feat: add more env types

--- apps/server/types/env.d.ts
+++ apps/server/types/env.d.ts
@@ -9,6 +9,14 @@ declare global {
       CLIENT_ORIGIN: string;
       EMAIL_USER: string;
       EMAIL_PASS: string;
+      ROBOKASSA_MERCHANT_LOGIN: string;
+      ROBOKASSA_PASSWORD1: string;
+      ROBOKASSA_PASSWORD2: string;
+      ROBOKASSA_TEST_MODE: string;
+      ROBOKASSA_RESULT_URL_REQUEST_METHOD: "POST" | "GET";
+      S3_BUCKET_NAME: string;
+      S3_PUBLIC_KEY: string;
+      S3_SECRET_KEY: string;
     }
   }
 }
