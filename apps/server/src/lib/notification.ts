35f9b780284a7a0a83a7c5bee5a949d1e7f87be0
Ansat Euler <iffypixy@gmail.com>
2024-01-07 17:16:17 +0100

    feat: add notification lib

---
+++ apps/server/src/lib/notification.ts
@@ -0,0 +1,9 @@
+export const NOTIFICATION_EVENTS = {
+  TASK_ACCEPTED: "task-accepted",
+  TASK_ASSIGNED: "task-assigned",
+  REQUEST_ACCEPTED: "request-accepted",
+  REQUEST_DECLINED: "request-declined",
+  REVIEW_GIVEN: "review-given",
+  KICKED_FROM_PROJECT: "kicked-from-project",
+  PROJECT_REQUEST_SENT: "project-request-sent",
+};
