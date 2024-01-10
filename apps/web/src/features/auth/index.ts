a3760387f09a69dc061f25fe35da675b9f731556
Ansat Euler <iffypixy@gmail.com>
2024-01-10 22:47:53 +0100

    chore: prepbuild@2

--- apps/web/src/features/auth/index.ts
+++ apps/web/src/features/auth/index.ts
@@ -1,4 +1,4 @@
 export {AuthenticationTemplate} from "./ui";
-export {useAuthStore} from "./store";
+// export {useAuthStore} from "./store";
 export {CredentialsLoader} from "./lib/credentials-loader";
 export {model as authModel} from "./model";
--- apps/web/src/features/auth/store.ts
+++ apps/web/src/features/auth/store.ts
@@ -1,15 +1,15 @@
-import {create} from "zustand";
+// import {create} from "zustand";
 
-import {Credentials} from "@shared/api";
-import {Nullable} from "@shared/lib/types";
+// import {Credentials} from "@shared/api";
+// import {Nullable} from "@shared/lib/types";
 
-interface AuthStore {
-  credentials: Nullable<Credentials>;
-  setCredentials: (credentials: Credentials) => void;
-}
+// interface AuthStore {
+//   credentials: Nullable<Credentials>;
+//   setCredentials: (credentials: Credentials) => void;
+// }
 
-export const useAuthStore = create<AuthStore>((set) => ({
-  credentials: null,
-  setCredentials: (credentials: Credentials) =>
-    set((state: AuthStore) => ({...state, credentials})),
-}));
+// export const useAuthStore = create<AuthStore>((set) => ({
+//   credentials: null,
+//   setCredentials: (credentials: Credentials) =>
+//     set((state: AuthStore) => ({...state, credentials})),
+// }));
